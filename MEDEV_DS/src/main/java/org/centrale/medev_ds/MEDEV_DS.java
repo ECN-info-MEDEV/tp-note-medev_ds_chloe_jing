f2ae20c97390d39b8a9ff91328bf57a8d3708332
chloe-caville <chloe.caville@gmail.com>
2024-01-10 15:53:57 +0100

    Add Netbeans project init

---
+++ MEDEV_DS/src/main/java/org/centrale/medev_ds/MEDEV_DS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.medev_ds;
+
+/**
+ *
+ * @author chloe
+ */
+public class MEDEV_DS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
