6de190af234777f26960f1641103a17235818d6c
chloe-caville <chloe.caville@gmail.com>
2024-01-11 09:00:45 +0100

    set up test

---
+++ DS_MEDEV/src/main/java/sample/ds_medev/DS_MEDEV.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sample.ds_medev;
+
+/**
+ *
+ * @author chloe
+ */
+public class DS_MEDEV {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
