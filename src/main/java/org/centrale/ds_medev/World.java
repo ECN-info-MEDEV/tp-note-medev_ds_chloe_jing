005f50ec19a897186fbf6314b396a507ec84f87e
Jing ZENG <144108743+JingZENG123@users.noreply.github.com>
2024-01-18 10:09:00 +0100

    modify lass World

--- src/main/java/org/centrale/ds_medev/World.java
+++ src/main/java/org/centrale/ds_medev/World.java
@@ -17,6 +17,57 @@ public class World {
     private Case2D[][] grid;
     private Player turn;
     private boolean gameOver;
+
+    public int getGridSize() {
+        return gridSize;
+    }
+
+    public void setGridSize(int gridSize) {
+        this.gridSize = gridSize;
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
+    }
+
+    public Case2D[][] getGrid() {
+        return grid;
+    }
+
+    public void setGrid(Case2D[][] grid) {
+
+    }
+
+    public Player getTurn() {
+        return turn;
+    }
+
+    public void setTurn(Player turn) {
+        this.turn = turn;
+    }
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
+    public void setGameOver(boolean gameOver) {
+        this.gameOver = gameOver;
+    }
+    
+    
+    
     public World(int gridSize){
         this.gridSize=gridSize;
         grid=new Case2D[gridSize][gridSize];
@@ -25,7 +76,7 @@ public class World {
     private void initializeGrid() {
         for (int i = 0; i < gridSize; i++) {
             for (int j = 0; j < gridSize; j++) {
-                grid[i][j] = new Case2D(i, j);
+                grid[i][j] = new Case2D(i, j,0);
             }
         }
     }
