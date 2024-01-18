cb0a103d8e93b16be927490e86208a7e2f3ed76b
chloe.caville@gmail.com <chloe.caville@gmail.com>
2024-01-18 10:42:42 +0100

    Improved the class Player

--- src/main/java/org/centrale/ds_medev/Player.java
+++ src/main/java/org/centrale/ds_medev/Player.java
@@ -5,28 +5,30 @@
 package org.centrale.ds_medev;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
- *
+ * Class for a player of the game
  * @author chloe et Jing
  */
 public class Player {
+    
     private String name;
-    private List<Case2D> grid;
+    
     private List<Ship> ships;
+    
+    //Constructor
+    
     public Player(String name){
         this.name = name;
-        this.grid = new ArrayList<>();
         this.ships = new ArrayList<>();
     }
-    public Player(){
-        
-    }
+    
     public String getName(){
         return name;
     }
     
-    public void setName(){
+    public void setName(String name){
         this.name=name;
     }
     
@@ -37,22 +39,42 @@ public class Player {
     public void setShips(List<Ship> ships) {
         this.ships = ships;
     }
-
-    public List<Case2D> getGrid() {
-        return grid;
-    }
     
-    public void placeShips(Ship ship){
+    //Methods
+    
+    /**
+     * Method to place the ships in the player's grid
+     * @param ship 
+     */
+    public void placeShips(World w){
+        Scanner scanner = new Scanner(System.in);
         
-    }
-    public void shoot(int x,int y){
+        System.out.println("This grid is of size 5x5. You have 3 ships to place, of size 3x1, 4x1, 5x1.");
+        System.out.println("Please place your ships on the grid by entering the coordinates one by one.");
+            
+        }
+    
+    public void shoot(World w){
+       Scanner scanner = new Scanner(System.in);
         
-    }
-    public void attack(Case2D position){
-        
-    }
-    public void addGridCell(){
-        
+       System.out.println("This grid is of size 5x5. You can choose a position to shoot at from [1,1] to [5,5].");
+       System.out.println("X coordinate for the attack :");
+       int x = scanner.nextInt();
+       System.out.println("Y coordinate for the attack :");
+       int y = scanner.nextInt();
+       
+       //Choose the grid to attack
+       Case2D[][] grid;
+       if (this.name.equals("Player 1")){
+           grid=w.getGrid2();
+       }
+       else{
+           grid=w.getGrid1();
+       }
+       
+       Case2D pos=grid[x][y];
+       //We change the state fo the case to attacked
+       pos.setCaseState(1);
     }
     //Adds a ship to the player's ship list
     public void addShip(Ship ship){
@@ -60,7 +82,7 @@ public class Player {
     }
     public boolean hasShipLeft(){
         for(Ship ship:ships){
-            if(!ship.isSunk()){
+            if(ship.getShipState()==0){
                 return true;
             }
         }
