2f81ef83e60b4fb475202045cc27fe0b5f835a1c
Jing ZENG <144108743+JingZENG123@users.noreply.github.com>
2024-01-18 09:53:06 +0100

    add class player

--- src/main/java/org/centrale/ds_medev/Player.java
+++ src/main/java/org/centrale/ds_medev/Player.java
@@ -3,11 +3,69 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package org.centrale.ds_medev;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  *
- * @author chloe
+ * @author chloe et Jing
  */
 public class Player {
+    private String name;
+    private List<Case2D> grid;
+    private List<Ship> ships;
+    public Player(String name){
+        this.name = name;
+        this.grid = new ArrayList<>();
+        this.ships = new ArrayList<>();
+    }
+    public Player(){
+        
+    }
+    public String getName(){
+        return name;
+    }
     
+    public void setName(){
+        this.name=name;
+    }
+    
+    public List<Ship> getShips() {
+        return ships;
+    }
+
+    public void setShips(List<Ship> ships) {
+        this.ships = ships;
+    }
+
+    public List<Case2D> getGrid() {
+        return grid;
+    }
+    
+    public void placeShips(Ship ship){
+        
+    }
+    public void shoot(int x,int y){
+        
+    }
+    public void attack(Case2D position){
+        
+    }
+    public void addGridCell(){
+        
+    }
+    //Adds a ship to the player's ship list
+    public void addShip(Ship ship){
+        ships.add(ship);
+    }
+    public boolean hasShipLeft(){
+        for(Ship ship:ships){
+            if(!ship.isSunk()){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+
 }
--- src/main/java/org/centrale/ds_medev/World.java
+++ src/main/java/org/centrale/ds_medev/World.java
@@ -3,11 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package org.centrale.ds_medev;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  *
  * @author chloe
  */
 public class World {
-    
+    private int gridSize;
+    private Player player1;
+    private Player player2;
+    private Case2D[][] grid;
+    private Player turn;
+    private boolean gameOver;
+    public World(int gridSize){
+        this.gridSize=gridSize;
+        grid=new Case2D[gridSize][gridSize];
+        initializeGrid();
+    }
+    private void initializeGrid() {
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                grid[i][j] = new Case2D(i, j);
+            }
+        }
+    }
+
 }
